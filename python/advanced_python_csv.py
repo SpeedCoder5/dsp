0afa29c3dd7ec99487bcc282edbfee6db6b0f994
SpeedCoder5 <07114GitHub>
2015-12-26 15:54:04 -0500

    05b Part 2

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,11 @@
-PLACE YOUR CODE HERE
+import csv
+
+rFilename = 'faculty.csv'
+wFilename = 'emails.csv'
+
+csv.register_dialect('whitespace', delimiter=',', skipinitialspace=True)
+with open(rFilename) as fr:
+    rdr = csv.DictReader(fr, dialect='whitespace')
+    with open(wFilename, 'w') as fw:
+        for row in rdr:
+            fw.write(row['email'] + '\n')
